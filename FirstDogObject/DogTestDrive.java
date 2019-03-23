7ee7283529fc873dc780af76910644830027d08a
Raul Padilla <raulpadilla112@gmail.com>
2019-03-22 23:00:17 -0400

    First time working on OO with Java(not really....)

---
+++ FirstDogObject/DogTestDrive.java
@@ -0,0 +1,9 @@
+
+class DogTestDrive{
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.size = 40;
+        System.out.println("My size is " + d.size  + " inches tall!!");
+        d.bark();
+    }
+}
