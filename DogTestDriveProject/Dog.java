fdac7c9e87a990b6679ae5035e5040ac0dbed8f3
Raul Padilla <raulpadilla112@gmail.com>
2019-03-28 23:50:54 -0400

    Playing around with the instance variable.

---
+++ DogTestDriveProject/Dog.java
@@ -0,0 +1,15 @@
+
+class Dog{
+    int size;
+    String name;
+
+    void bark(){
+        if(size > 50){
+            System.out.println("Wooof! Wooof!");
+        }else if (size >14){
+            System.out.println("Ruff! Ruff!");
+        }else{
+            System.out.println("Yip! Yip!");
+        }
+    }
+}
