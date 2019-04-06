52effd22b8edc4c853301d5ba453ced4b6690a0c
Raul Padilla <raulpadilla112@gmail.com>
2019-04-05 23:37:09 -0400

    Fixed an error in which I replaced array with an ArrayList

--- SimpleDotComProject/SimpleDotCom.java
+++ SimpleDotComProject/SimpleDotCom.java
@@ -1,26 +1,25 @@
-public class SimpleDotCom{
-    int[] locationCells;
-    int numOfHits = 0;
+import java.util.ArrayList;
 
-    public void setLocationCells(int[] locs){
+public class SimpleDotCom{
+    private ArrayList<String> locationCells;
+
+    public void setLocationCells(ArrayList<String> locs){
         locationCells = locs;
     }
 
-    public String checkYourself(String stringGuess){
-        int guess = Integer.parseInt(stringGuess);
-        String result = "miss";
-        for(int cell : locationCells){
-            if(guess == cell){
-                result = "hit";
-                numOfHits++;
-                break;
-            }
-        }
+    public String checkYourself(String userInput){
+       String result = "miss";
+       int index = locationCells.indexOf(userInput);
+       if(index>=0){
+           locationCells.remove(index);
 
-        if(numOfHits == locationCells.length){
-            result = "kill";
-        }
-        System.out.println(result);
-        return result;
+           if(locationCells.isEmpty()){
+               result = "kill";
+           }else{
+               result = "hit";
+           }
+           
+       }
+       return result;
     }
 }
