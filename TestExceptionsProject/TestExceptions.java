01e0a02158bf3db55e7ac53f02df558a0a29723e
Raul Padilla <raulpadilla112@gmail.com>
2019-06-06 12:56:59 -0400

    Testing out how exceptions work.

---
+++ TestExceptionsProject/TestExceptions.java
@@ -0,0 +1,24 @@
+public class TestExceptions{
+    public static void main(String[] args) {
+        String test = "no";
+        try{
+            System.out.println("start try");
+            doRisky(test);
+            System.out.println("end try");
+        }catch(Exception se){
+            System.out.println("scary exception");
+        }finally{
+            System.out.println("end of main");
+        }    
+    }
+
+    static void doRisky(String test)throws Exception{
+        System.out.println("start risky");
+        if("yes".equals(test)){
+            throw new Exception();
+        }
+        System.out.println("end risky");
+        return;
+    }
+    
+}
