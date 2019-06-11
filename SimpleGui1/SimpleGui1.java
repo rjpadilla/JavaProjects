931f5dc779db7fc631da1a155255b770e070b578
Raul Padilla <raulpadilla112@gmail.com>
2019-06-10 20:16:52 -0400

    Added event handler.

--- SimpleGui1/SimpleGui1.java
+++ SimpleGui1/SimpleGui1.java
@@ -1,16 +1,27 @@
 import javax.swing.*;
+import java.awt.event.*;
 
-public class SimpleGui1 {
+public class SimpleGui1 implements ActionListener {
+
+    JButton button;
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("click me");
+        SimpleGui1 gui = new SimpleGui1();
+        gui.go();
+    }
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public void go(){
+        JFrame frame = new JFrame();
+        button = new JButton("click me");
+
+        button.addActionListener(this);
 
         frame.getContentPane().add(button);
-
-        frame.setSize(300, 300);
-
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
         frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent event){
+        button.setText("I've been clicked!");
     }
 }
