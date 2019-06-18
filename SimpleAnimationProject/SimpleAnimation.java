7ea2519624e5053abe66cba8e9d3b2781e6a56cc
Raul Padilla <raulpadilla112@gmail.com>
2019-06-18 02:42:24 -0400

    Fixed an error with animation trailing.

--- SimpleAnimationProject/SimpleAnimation.java
+++ SimpleAnimationProject/SimpleAnimation.java
@@ -34,6 +34,9 @@ public class SimpleAnimation {
 
     class MyDrawPanel extends JPanel{
         public void paintComponent(Graphics g){
+            g.setColor(Color.white);
+            g.fillRect(0,0,this.getWidth(), this.getHeight());
+            
             g.setColor(Color.green);
             g.fillOval(x,y,40,40);
         }
