391ae5eacf994147e42167d8052c546612ab566f
Raul Padilla <raulpadilla112@gmail.com>
2019-06-29 21:25:37 -0400

    Java GUI that creates flashcards.

---
+++ QuizCardProject/QuizCard.java
@@ -0,0 +1,18 @@
+
+public class QuizCard {
+
+    private String question;
+    private String answer;
+
+    public QuizCard(String q, String a) {
+
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
