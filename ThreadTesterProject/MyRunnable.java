f044e02b205615685776064bc4277a45e4c3b096
Raul Padilla <raulpadilla112@gmail.com>
2019-07-09 11:05:18 -0400

    Updated with sleep method.

--- ThreadTesterProject/MyRunnable.java
+++ ThreadTesterProject/MyRunnable.java
@@ -5,6 +5,11 @@ public class MyRunnable implements Runnable{
     }
 
     public void go(){
+        try{
+            Thread.sleep(2000);
+        }catch(InterruptedException ex){
+            ex.printStackTrace();
+        }
         doMore();
     }
 
