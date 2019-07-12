2e64a9f8cf42ae2342b987a18058db42c053b8bb
Raul Padilla <raulpadilla112@gmail.com>
2019-07-11 21:04:43 -0400

    Added synch to withdrawal method.

--- SynchThreadsProject/RyanAndMonicaJob.java
+++ SynchThreadsProject/RyanAndMonicaJob.java
@@ -20,7 +20,7 @@ public class RyanAndMonicaJob implements Runnable {
         }
     }
 
-    private void makeWithdrawl(int amount) {
+    private synchronized void makeWithdrawl(int amount) {
         if (account.getBalance() >= amount) {
             System.out.println(Thread.currentThread().getName() + " is about to withdraw");
             try {
