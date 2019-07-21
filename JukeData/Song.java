99d974b54775c3047cb2450ff32f388c0f41ee21
Raul Padilla <raulpadilla112@gmail.com>
2019-07-20 21:51:29 -0400

    Overridden equals and hashcode.

--- JukeData/Song.java
+++ JukeData/Song.java
@@ -1,37 +1,46 @@
-class Song implements Comparable<Song>{
+class Song implements Comparable<Song> {
     String title;
     String artist;
     String rating;
     String bpm;
 
-    public int compareTo(Song s){
+    public boolean equals(Object aSong) {
+        Song s = (Song) aSong;
+        return getTitle().equals(s.getTitle());
+    }
+
+    public int hashCode(){
+        return title.hashCode();
+    }
+
+    public int compareTo(Song s) {
         return title.compareTo(s.getTitle());
     }
 
-    Song(String t, String a, String r, String b){
+    Song(String t, String a, String r, String b) {
         title = t;
         artist = a;
         rating = r;
         bpm = b;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public String getArtist(){
+    public String getArtist() {
         return artist;
     }
 
-    public String getRating(){
+    public String getRating() {
         return rating;
     }
 
-    public String getBpm(){
+    public String getBpm() {
         return bpm;
     }
 
-    public String toString(){
+    public String toString() {
         return title;
     }
 }
